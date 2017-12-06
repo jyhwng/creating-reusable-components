54c44180587f8d1df07da860069b17b7a54aeff3
텀블벅_자산 <teombeulbeog_jasan@teombeulbeog-jasan-ui-MacBook-Pro.local>
2017-12-06 14:44:24 +0900

    Create atomic, molecule components & add example components

---
+++ src/Copy.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Copy extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            password: ''
+        }
+    }
+    render() {
+        return (
+            <div/>
+        )
+    }
+}
+
+Copy.propTypes = {
+
+}
+
+Copy.defaultProps = {
+    
+}
+
+export default Copy;
---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+/** Required label */
+export default function ExampleOptional() {
+    return <Label htmlFor="testhtml" label="testlabel" required />
+}
